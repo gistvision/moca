3540dd395e6b54959cb995964b360f12fea23597
Byeonghwi Kim <byeonghwikim@gmail.com>
2020-12-09 21:23:30 +0900

    Delete download_pretrained_weight.sh

--- exp/pretrained/download_pretrained_weight.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Download, Unzip, and Remove zip
-echo "Downloading Pretrained MOCA Weight ..."
-wget https://alfred-colorswap.s3.us-east-2.amazonaws.com/pretrained.pth
-echo "Done."
-
